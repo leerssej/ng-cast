dd4d4e27a19926387f8c1cc0d79720cc56330d30
Hack Reactor Student <student@hackreactor.com>
2018-06-29 15:55:21 -0700

    render video list titles

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,4 +1,9 @@
 angular.module('video-player')
   .component('videoListEntry', {
-    templateUrl: 'src/templates/videoListEntry.html'
+    templateUrl: 'src/templates/videoListEntry.html',
+  
+    controller: function($scope) {
+      this.video = $scope.$parent.video;
+    }
+  
   });
