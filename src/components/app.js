ed845ae4047c9f0919b09b6b6dc39de57b7ac17f
Hack Reactor Student <student@hackreactor.com>
2018-06-30 11:03:14 -0700

    marks point where nested searchFunctionality works perfectly but does not pass tests TODO we are going to unnest the functions so that they pass tests for discrete functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,20 @@
 angular.module('video-player')//.service('youTube', youTube)
-  
   .controller('myController', ['youTube', function(youTube) {
-    this.videos = window.exampleVideoData;
-    this.currentVideo = window.exampleVideoData[0];
+    // this.videos = window.exampleVideoData;
+    // this.currentVideo = window.exampleVideoData[0];
+    this.videos = [];
+    this.currentVideo = {};
       
     this.selectVideo = video => {
       this.currentVideo = video;
     };
       
-    //youTube();
-    this.search = q => youTube.searchYouTube(q, (data) => {
-      // console.log(this.videos);
+    this.searchResults = q => youTube.searchYouTube(q, (data) => {
       this.videos = data;
       this.currentVideo = data[0];
     });
+
+    this.searchResults();
   }])
 
   .component('app', {   
