e77c61e755d4f35cc1bc1a7a7651c0f52c0f2904
Hack Reactor Student <student@hackreactor.com>
2018-06-29 20:19:24 -0700

    constructs you tube service api

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,17 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
-});
+  .service('youTube', function() {
+    config = {
+      part: 'snippet',
+      key: window.YOUTUBE_API_KEY,
+      maxResults: 5,
+      q: 'aman vaid',
+      videoEmbeddable: true
+    },
+    $http.get('https://www.googleapis.com/youtube/v3/search', config)
+      .then(function(data) {
+        console.log(data);
+      },
+      function(error) {
+        console.log(error);
+      });
+  });
